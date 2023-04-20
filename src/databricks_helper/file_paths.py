38ca5bf19a746b3d6c4d826068dda4332677478e
AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>
2023-04-20 11:05:30 -0500

    Update file_paths.py

--- src/databricks_helper/file_paths.py
+++ src/databricks_helper/file_paths.py
@@ -73,11 +73,11 @@ def list_file_paths(dir_path, ext='csv', path_type='os'):
         if not path_exists(dir_path):
             print(f'Directory not found: {dir_path}')
             return []
-        if path_type=='os:
+        if path_type =='os':
             fps = [db_path_to_local(f.path) 
                     for f in dbutils.fs.ls(dir_path) 
                     if ((f.path).lower()).endswith(f'.{ext.lower()}')]
-        elif path_type=='dbfs:
+        elif path_type =='dbfs':
             fps = [f.path 
                     for f in dbutils.fs.ls(dir_path) 
                     if ((f.path).lower()).endswith(f'.{ext.lower()}')]
