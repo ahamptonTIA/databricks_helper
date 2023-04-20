2095fe8a8ba25091cea1b3412faf91da3fd8117d
AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>
2023-04-20 09:25:59 -0500

    initial commit

--- setup.py
+++ setup.py
@@ -1,1 +1,39 @@
 
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='databricks_helper',               # name of the package
+    version='0.0.1',                        # release version
+    author='ahamptonTIA',                   # org/author
+    description=\
+        '''
+        databricks_helper
+
+        The databricks_helper package is a collection of helper functions
+        used within databricks.   
+         
+        ''',
+    long_description=long_description,      # long description read from the the readme file
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),    # list of all python modules to be installed
+    classifiers=[                           # information to filter the project on PyPi website
+                        'Programming Language :: Python :: 3',
+                        'License :: OSI Approved :: MIT License',
+                        'Operating System :: OS Independent',
+                        'Natural Language :: English',
+                        'Programming Language :: Python :: 3.9',
+                        ],                                      
+    python_requires='>=3.9',                # minimum version requirement of the package
+    py_modules=['databricks_helper'],       # name of the python package
+    package_dir={'':'src'},                 # directory of the source code of the package
+    install_requires=[                      # package dependencies
+                        'pyspark>=3.3.0',
+                        're>=2.2.1', 
+                        'os',
+                        'multiprocessing',
+                        'math'
+                    ]
+    )
