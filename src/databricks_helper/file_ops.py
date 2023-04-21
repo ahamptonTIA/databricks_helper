05340b1d588b03b9c038dd8f13c1c96c90229119
AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>
2023-04-21 12:18:07 -0500

    Update file_ops.py

--- src/databricks_helper/file_ops.py
+++ src/databricks_helper/file_ops.py
@@ -1,6 +1,6 @@
 import os, re, math, uuid
 import hashlib
-from tqdm import tqdm
+from datetime import datetime, timezone
 from multiprocessing.pool import ThreadPool
 from multiprocessing import cpu_count
 
@@ -45,17 +45,16 @@ def get_md5_hash(file):
             f_hash.update(chunk)
     return f_hash.hexdigest() 
 #----------------------------------------------------------------------------------
-def get_file_details(dbutils, dir_path, id_col,spark=None):
-    """Function returns a pyspark sql dataframe that details
-    the file name, size (bytes), total record counts, and 
-    a count by unique values in a field. 
+def get_csv_file_details(dbutils, file_path, id_col, spark=None):
+    """Function returns a dictionary that details
+    general metadata for a csv file
 
     Parameters
     ----------
     dbutils: dbutils object
         DataBricks notebook dbutils object
-    dir_path : str
-        DataBricks file storage path
+    file_path : str
+        DataBricks file storage path to a csv
     id_col : str
         Column name for a column that holds an ID or
         set of values to count distinct values of. 
@@ -63,46 +62,39 @@ def get_file_details(dbutils, dir_path, id_col,spark=None):
         Defualt, in not supplied a new session will be built
     Returns
     ----------
-    df: pyspark.sql.dataframe.DataFrame
-        Dataframe that details the file name, 
-        size (bytes), total record counts, and 
-        a count by unique values in a field.
+    file_meta: dict
+        dictionary of file metadata
     """
-    
     if not spark:
         from pyspark.sql import SparkSession
         spark = SparkSession.builder.appName(uuid.uuid4().hex).getOrCreate()
-        
-    schema = f"""
-                file_name STRING, 
-                file_size_bytes LONG,
-                file_size_memory_unit STRING, 
-                record_qty LONG,
-                {id_col}_qty LONG,
-                file_md5_hash STRING
-              """     
-    data = []
 
-    files = dbutils.fs.ls(dir_path)
-    file_cnt = len(files)
-    
-    for f in tqdm(files, desc="Evaluating Files..."):
-        sdf = spark.read.csv(f.path,
-                              header=True)
-        os_fp = dbfs_path.db_path_to_local(f.path)
-        data.append(
-                      (
-                        f.name,
-                        f.size,
-                        get_byte_units(int(f.size)),                      
-                        sdf.count(),
-                        sdf.select(id_col).distinct().count(),
-                        get_md5_hash(os_fp)
-                      )
-                    )
-    df = spark.createDataFrame(data=data,schema=schema)
-    
-    return(df)
+    # get a dbruntime.dbutils.FileInfo object
+    f = dbutils.fs.ls(dbfs_path.to_dbfs_path(fps[0]))[0]
+
+    # get the local/os file path
+    os_fp = dbfs_path.db_path_to_local(f.path)
+
+    # get date time file metadata
+    statinfo = os.stat(os_fp)
+    create_date = datetime.fromtimestamp(statinfo.st_ctime).isoformat()
+    modified_date = datetime.fromtimestamp(statinfo.st_mtime).isoformat()
+
+    # read the csv data into a spark dataframe
+    sdf = spark.read.csv(f.path, header=True)
+
+    # create dictionary to store the metadata
+    file_meta = {
+                    'file_name': f.name,
+                    'file_size_bytes': f'{f.size:,}',
+                    'file_size_memory_unit': get_byte_units(int(f.size)),
+                    'record_qty': f'{sdf.count():,}',   
+                    f'{id_col}_qty': f'{sdf.select(id_col).distinct().count():,}',
+                    'file_md5_hash': get_md5_hash(os_fp),
+                    'created' : create_date,
+                    'modified' : modified_date
+                }    
+    return file_meta 
 #---------------------------------------------------------------------------------- 
 def regex_file_pattern_sub(file, sub_dict):
     """Function searches a file for regex string pattern matches
@@ -164,14 +156,18 @@ def regex_file_pattern_sub_mcp(files, sub_dict, n_cores=None):
         Dictionary of regex key, value pairs where the key
         is the pattern to match and the value is string to
         replace the matched pattern.
+    n_cores : int
+        Number of cores to use.  Will not exceed 85% of 
+        available cores.
+        Default, < 85% of the total cores available
     Returns
     ----------
     list of dictionaries
-        Dictionary where the key is the file path and the value
+        list of dictionaries where the key is the file path and the value
         is a nested dictionary of the match patterns, substituted 
         value, and a count of matches.
         The dictionary can be used to track file modifications. 
-        [{file_path1: {pattern : substitution, count}}]
+        [{file_path: {pattern : {substitution : x , count: y}}]
     """     
     max_cores = math.floor(cpu_count() * 0.85)
     if not n_cores:
@@ -185,6 +181,53 @@ def regex_file_pattern_sub_mcp(files, sub_dict, n_cores=None):
     task_params = [(f, sub_dict) for f in files]
     try:
         result = pool.starmap(regex_file_pattern_sub, task_params)
+        pool.close()
+        pool.join()
+    except Exception as e:
+        print(f'Error: {e}')
+        pool.terminate()
+    return result
+#---------------------------------------------------------------------------------- 
+def get_csv_file_details_mcp(dbutils, files, id_col, n_cores=None, spark=None):   
+    """Function uses multi-core processing to call regex_file_pattern_sub.
+    See regex_file_pattern_sub documentation.
+    Parameters
+    ----------
+     dbutils: dbutils object
+        DataBricks notebook dbutils object   
+    files : list
+        List of string csv file paths
+    id_col : str
+        Column name for a column that holds an ID or
+        set of values to count distinct values of. 
+    n_cores : int
+        Number of cores to use.  Will not exceed 85% of 
+        available cores.
+        Default, < 85% of the total cores available      
+    spark : spark session object
+        Defualt, in not supplied a new session will be built
+    Returns
+    ----------
+    list of dictionaries
+        Dictionary where the key is the file path and the value
+        is a nested dictionary of the match patterns, substituted 
+        value, and a count of matches.
+        The dictionary can be used to track file modifications. 
+        [{file_path1: {pattern : substitution, count}}]
+    """     
+
+    max_cores = math.floor(cpu_count() * 0.85)
+    if not n_cores:
+        n_cores = max_cores
+    elif n_cores > max_cores:
+        n_cores = max_cores
+
+    print(f"Using {n_cores} cores's of {cpu_count()}")
+    pool = ThreadPool(n_cores)
+    
+    task_params = [(dbutils, f, id_col, spark) for f in files]
+    try:
+        result = pool.starmap(get_csv_file_details, task_params)
         pool.close()
         pool.join()
     except Exception as e:
