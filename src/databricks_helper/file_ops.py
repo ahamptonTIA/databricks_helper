16ba0fa5eca154352d1e6792ec7dda4a2f058e6f
AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>
2023-06-21 07:26:43 -0500

    Update file_ops.py changes kb size

--- src/databricks_helper/file_ops.py
+++ src/databricks_helper/file_ops.py
@@ -35,7 +35,7 @@ def get_byte_units(size_bytes):
     if size_bytes == 0:
         return '0 : B'
     size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = int(math.floor(math.log(size_bytes, 1024)))
+    i = int(math.floor(math.log(size_bytes, 1000))) #1024
     p = math.pow(1024, i)
     s = round(size_bytes / p, 2)
     return f'{s} : {size_name[i]}'
