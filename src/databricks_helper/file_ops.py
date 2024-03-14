3e61feb7c47d9d784676f3fdf7cf726489cc5015
AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>
2024-03-14 15:49:59 -0500

    updated get_byte_units to 1000 from 1024

--- src/databricks_helper/file_ops.py
+++ src/databricks_helper/file_ops.py
@@ -37,8 +37,8 @@ def get_byte_units(size_bytes):
     if size_bytes == 0:
         return '0 : B'
     size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = int(math.floor(math.log(size_bytes, 1000))) #1024
-    p = math.pow(1024, i)
+    i = int(math.floor(math.log(size_bytes, 1000))) 
+    p = math.pow(1000, i)
     s = round(size_bytes / p, 2)
     return f'{s} : {size_name[i]}'
 #----------------------------------------------------------------------------------
