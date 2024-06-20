47adad118b496f2f0e7a4cc1235f17f9559b9a86
AHampton NTIA <115647832+ahamptonTIA@users.noreply.github.com>
2024-06-20 10:08:13 -0500

    requires 'pandas>=1.1.0,<2.1.0',    'numpy<1.25,>=1.16.0',

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setuptools.setup(
     package_dir={'':'src'},                 # directory of the source code of the package
     packages=setuptools.find_packages(where="src"), # list of all python modules to be installed
     install_requires=[           # package dependencies
-                     'openpyxl',
-                     'numpy<=1.26.4'
-
+                        'pandas>=1.1.0,<2.1.0',  
+                        'numpy<1.25,>=1.16.0',
+                        'openpyxl',
                     ]
     )
